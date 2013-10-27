db099f65aab63ad3a9c141d439a5d4cb8216b2a9
Greg <bronevetsky1@llnl.gov>
2013-10-26 22:58:43 -0700

    Generalized structured file processing so that multiple types of data streams can be parsed and turned into a layout or restructured. Also some work to add the working directory and environment to the structure file.
    currently debugging the new capability

---
+++ process.C
@@ -0,0 +1,391 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <map>
+#include <list>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <errno.h>
+#include "process.h"
+using namespace std;
+
+//#define VERBOSE
+namespace sight {
+/***************************
+ ***** structureParser *****
+ ***************************/
+
+template<typename streamT>
+structureParser<streamT>::structureParser(int bufSize) : bufSize(bufSize) {
+}
+
+template<typename streamT>
+structureParser<streamT>::structureParser(streamT* stream, int bufSize) : bufSize(bufSize) {
+  init(stream);
+}
+
+template<typename streamT>
+void structureParser<streamT>::init(streamT* stream) {
+  this->stream = stream;
+  buf = new char[bufSize];
+  assert(buf);
+  
+  // Start reading the file, filling as much of buf as possible. Store the number of
+  // bytes read in dataInBuf and initialize bufIdx to refer to the start of buf.
+  //cout << "f="<<f<<", feof(f)="<<feof(f)<<", ferror(f)="<<ferror(f)<<", sizeof(buf)="<<sizeof(buf)<<endl;
+  dataInBuf = readData();
+  bufIdx=0;
+  
+  loc = start;
+}
+
+template<typename streamT>
+pair<typename structureParser<streamT>::tagType, const properties&> structureParser<streamT>::next() {
+  bool success = true;
+  string readTxt; // String where text read by readUntil() will be placed
+  char termChar;  // Character where readUntil() places the character that caused parsing to terminate
+  string tagName; // The name of the current tag
+  string propName, propVal;
+  long int numProps;
+  bool derived;
+  
+  std::map<std::string, std::string> pMap;
+  
+  // Reset tagProperties in preparation of a new tag being read
+  tagProperties.clear();
+  
+  // When we enter this function we resume text processing from where we left off
+  // in the last call to getNext(). loc records this location and we now jump to it.
+  switch(loc) {
+    textRead:     goto TEXT_READ_LOC;
+    enterTagRead: goto ENTER_TAG_READ_LOC;
+    exitTagRead:  goto EXIT_TAG_READ_LOC;
+    done:         goto DONE_LOC;
+  }
+  
+  while(success) {
+//cout << "main loop, bufIdx="<<bufIdx<<", bufSize="<<bufSize<<endl;
+    // We must currently be outside of a tag, although we may be between the multiple individual
+    // tags that encode entry into an object with a multi-level inheritance hierarchy (multiple tags)
+
+    // Look for the start of the next tag
+    success = readUntil(true, "[", 1, termChar, readTxt);
+    // Emit the text before the start of the tag
+    #ifdef VERBOSE
+    cout << "text=\""<<readTxt<<"\""<<endl;
+    #endif
+    //dbg << readTxt;
+    
+    if(readTxt != "") {
+      std::map<std::string, std::string> pMap;
+      pMap["text"] = readTxt;
+      tagProperties.add("text", pMap);
+      loc = textRead;
+      return make_pair(enterTag, tagProperties);
+    }
+    
+    if(!success) goto DONE_LOC;
+      
+    TEXT_READ_LOC:
+
+    nextChar();
+
+    // Now look for the end of name of the tag (a whitespace char) or the / char 
+    // that indicates that this is the end rather than beginning of a tag
+    if(!(success = readUntil(true, " \t\r\n/|", 6, termChar, tagName))) goto DONE_LOC;
+
+    // If this is the end of a tag, read until its end to determine its name
+    if(termChar == '/') {
+      if(!nextChar()) goto DONE_LOC;
+
+      if(!(success = readUntil(true, "]", 1, termChar, tagName))) goto DONE_LOC;
+      //if(!(success = readUntil(true, " \t\r\n", 4, termChar, tagName))) goto DONE_LOC;
+
+      #ifdef VERBOSE
+      cout << "END \""<<tagName<<"\""<<endl;
+      #endif
+      
+      tagProperties.add(tagName, pMap);
+      loc = exitTagRead;
+      return make_pair(exitTag, tagProperties);
+      
+      EXIT_TAG_READ_LOC:
+      
+      if(!nextChar()) goto DONE_LOC;
+    } else {
+      // Records whether this entry tag corresponds to a base class that has been derived by another
+      derived=false;
+
+      //cout << "START "<<tagName<<" termChar="<<termChar<<endl;
+      if(termChar == '|') {
+        if(!nextChar()) goto DONE_LOC;
+        if(!(success = readUntil(true, " \t\r\n", 4, termChar, tagName))) goto DONE_LOC;
+
+        derived=true;
+      }
+      	
+      // Read the "numProperties" property
+      if(!(success = readProperty(propName, propVal, termChar))) { goto DONE_LOC; }
+      if(propName != "numProperties") { cerr<< "ERROR: expecting numProperties property in tag "<<tagName<<" but got \""<<propName<<"\"!"<<endl; exit(-1); }
+      numProps = strtol(propVal.c_str(), NULL, 10);
+//cout << "propName=\""<<propName<<"\", propVal=\""<<propVal<<"\", numProps="<<numProps<<", termChar=\""<<termChar<<"\""<<endl;
+
+      // Skip until the start of the next property or the end of the tag
+      if(!(success = readUntil(true, " \t\r\n]", 5, termChar, readTxt))) goto DONE_LOC;
+
+      // Read the properties of this tag
+      for(long int p=0; p<numProps; p++) {
+        // If we reached the end of the tag before processing all the properties
+        if(termChar==']') { cerr << "ERROR: reached the end of tag "<<tagName<<" after processing "<<p<<" properties but expected "<<numProps<<" properties!"<<endl; exit(-1); }
+//cout << "  prop "<<p<<": termChar=\""<<termChar<<"\""<<" buf["<<bufIdx<<"]=\""<<buf[bufIdx]<<"\""<<endl;
+
+        // Read the name/val pair that encodes the name of the current property
+        string propNameName, propNameVal;
+        if(!(success = readProperty(propNameName, propNameVal, termChar))) { goto DONE_LOC; }
+
+//cout << "  prop "<<p<<": "<<propNameName<<" = "<<propNameVal<<endl;
+
+        // Skip until the start of the next name/val pair
+        if(!(success = readUntil(true, " \t\r\n]", 5, termChar, readTxt))) goto DONE_LOC;
+        if(termChar==']') { cerr << "ERROR: reached the end of tag "<<tagName<<" reading the name of property "<<p<<" but expected "<<numProps<<" properties!"<<endl; exit(-1); }
+
+        // Read the name/val pair that encodes the name of the current property
+        string propValName, propValVal;
+        if(!(success = readProperty(propValName, propValVal, termChar))) { goto DONE_LOC; }
+
+//        cout << "  prop "<<p<<": "<<propValName<<" = "<<propValVal<<endl;
+
+        pMap[propNameVal] = propValVal;
+
+//        cout << "  prop "<<p<<": termChar=\""<<termChar<<"\""<<" buf["<<bufIdx<<"]=\""<<buf[bufIdx]<<"\""<<endl;
+
+        // Skip until the start of the next property or the end of the tag
+        if(!(success = readUntil(true, " \t\r\n]", 5, termChar, readTxt))) goto DONE_LOC;
+//        cout << "  prop "<<p<<": termChar=\""<<termChar<<"\""<<" buf["<<bufIdx<<"]=\""<<buf[bufIdx]<<"\""<<endl;
+      }
+      //cout << "end tag: termChar=\""<<termChar<<"\""<<" buf["<<bufIdx<<"]=\""<<buf[bufIdx]<<"\""<<endl;
+
+      // If we failed to reach the end of the tag after processing all the properties
+      if(termChar!=']') {
+        // Keep reading the remaining whitespace
+        if(!(success = readUntil(true, " \t\r\n", 4, termChar, readTxt))) goto DONE_LOC;
+
+        // This must be the end of the tag
+        if(termChar != ']')
+        { cerr << "ERROR: failed to reached the end of tag "<<tagName<<" after processing "<<numProps<<" properties! termChar=\""<<termChar<<"\""<<endl; exit(-1); }
+      }
+      if(!nextChar()) goto DONE_LOC;
+
+      // If this tag corresponds to an object at the outer-most level of a derivation hierarchy
+      // (it was not derived by another)
+      if(!derived) {
+        // Print out the properties
+        #ifdef VERBOSE
+        cout << "START "<<tagName<<endl;
+        #endif
+        tagProperties.add(tagName, pMap);
+        #ifdef VERBOSE
+        cout << tagProperties.str()<<endl;
+        #endif
+        
+        loc = enterTagRead;
+        return make_pair(enterTag, tagProperties);
+        
+        ENTER_TAG_READ_LOC:
+        ;
+      // If this tag's class was derived by another, add its properties to pStack so that it can be
+      // picked up when we reach the derived class' tag
+      } else
+        tagProperties.add(tagName, pMap);
+    }
+  }
+
+  DONE_LOC:
+  loc = done;
+  return make_pair(exitTag, tagProperties);
+}
+
+// Read a property name/value pair from the given file, setting name and val to them.
+// Reading starts at buf[bufIdx] and continues as far as needed, reading more file 
+// contents into buf if the end of buf is reached. bufSize is the number of bytes in 
+// the buffer. When the function returns bufIdx is updated to hold the current 
+// position inside buf, immediately after the terminator char was encountered and 
+// bufSize is updated to hold the total number of characters currently in buf. 
+// termChar is set to be the character that immediately follows the property name/value.
+// pair. This information enables the caller to determine whether the tag is finished.
+// or whether more properties exist in the tag. If readProperty() reads the full 
+// property info before it reaches the end of the file, it returns true and false otherwise.
+template<typename streamT>
+bool structureParser<streamT>::readProperty(std::string& name, std::string& val, char& termChar) {
+  string readTxt; // Generic string to hold read data
+
+  // Skip until the next non-whitespace character
+  if(!readUntil(false, " \t\r\n", 4, termChar, readTxt)) return false;
+  if(!nextChar()) return false;
+//cout << "    rp1: readTxt=\""<<readTxt<<"\" termChar=\""<<termChar<<"\""<<" buf[bufIdx]=\""<<buf[bufIdx]<<"\""<<endl;
+  
+  // Read until the next '='
+  if(!readUntil(true, "=", 1, termChar, name)) return false;
+  if(!nextChar()) return false;
+
+//cout << "    rp2: name=\""<<name<<"\", termChar=\""<<termChar<<"\""<<endl;
+  // Read until the next non-'='
+  if(!readUntil(false, "=", 1, termChar, readTxt)) return false;
+
+  //if(readTxt.length()!=1) { cerr << "ERROR: multiple = chars in property "<<name<<"! read \""<<readTxt<<"\" termChar=\""<<termChar<<"\""<<endl; return false; }
+//cout << "    rp3: readTxt=\""<<readTxt<<"\", termChar=\""<<termChar<<"\" buf[bufIdx]=\""<<buf[bufIdx]<<"\""<<endl;
+
+  // Read the " that immediately follows
+  if(!readUntil(true, "\"", 1, termChar, readTxt)) return false;
+  if(!nextChar()) return false;
+//cout << "    rp4: readTxt=\""<<readTxt<<"\", termChar=\""<<termChar<<"\" buf[bufIdx]=\""<<buf[bufIdx]<<"\""<<endl;
+
+  if(termChar!='\"' && readTxt.length()!=0) { cerr << "ERROR: erroneous chars between = and \" in tag property "<<name<<"! read \""<<readTxt<<"\" termChar=\""<<termChar<<"\""<<endl; return false; }
+
+  // Read until the following "
+  if(!readUntil(true, "\"", 1, termChar, val)) return false;
+//cout << "    rp5: val=\""<<val<<"\", termChar=\""<<termChar<<"\" buf[bufIdx]=\""<<buf[bufIdx]<<"\""<<endl;
+
+  return true;
+}
+
+// Reads all the characters from FILE* f until the next terminating character.
+// If inTerm is true, a terminating char is anything in termChars. If inTerm is 
+// false, a terminating char is anything not in termChars. readUntil() then sets 
+// result to the read text and sets termHit to the terminator char that was hit. 
+// Reading starts at buf[bufIdx] and continues as far as needed, reading more file 
+// contents into buf if the end of buf is reached. bufSize is the number of bytes in 
+// the buffer. When the function returns bufIdx is updated to hold the current 
+// position inside buf, immediately after the terminator char was encountered and 
+// bufSize is updated to hold the total number of characters currently in buf. If 
+// readUntil() finds a terminator char before it reaches the end of the file, it 
+// returns true and false otherwise.
+template<typename streamT>
+bool structureParser<streamT>::readUntil(bool inTerm, const char* termChars, int numTermChars, 
+                                char& termHit, string& result) {
+  result = "";
+  // Outer loop that keeps reading more chunks of size bufSize from the file
+  while(1) {
+    //cout << "        ru: bufIdx="<<bufIdx<<", bufSize="<<bufSize<<endl;
+    // Iterate through the rest of the file looking for terminator chars
+    while(bufIdx<dataInBuf) {
+      for(int i=0; i<numTermChars; i++) {
+        // If the current character in buf is a terminator, return the result
+        if(isMember(buf[bufIdx], termChars, numTermChars)) {
+          if(inTerm) { termHit=buf[bufIdx]; return true; }
+        } else {
+          if(!inTerm) { termHit=buf[bufIdx]; return true; }
+        }
+      }
+      // If the current character in buf is a terminator, return the result
+      if(!inTerm) {
+        termHit=buf[bufIdx];
+        //nextChar();
+        return true;
+      }
+      // If the character is not a terminator, append it to ret
+      result += buf[bufIdx];
+      bufIdx++;
+    }
+
+    //cout << "        ru: feof(f)="<<feof(f)<<", ferror(f)="<<ferror(f)<<endl;
+
+    // If we've reached the end of the file, return unsuccessfuly
+    if(streamEnd()) return false;
+
+    // If we've encountered an error, yell
+    if(streamError()) {
+      fprintf(stderr, "ERROR reading file!");
+      exit(-1);
+    }
+    
+    nextChar();
+  }
+}
+
+// Advances the current reading position by 1 char, reading file contents into buf 
+// if the end of buf is reached. bufSize is the number of bytes in 
+// the buffer. When the function returns bufIdx is updated to hold the current 
+// position inside buf, immediately after the terminator char was encountered and 
+// bufSize is updated to hold the total number of characters currently in buf. 
+// Returns the read char. If there is a next character in the file, returns true.
+// Otherwise, returns false.
+template<typename streamT>
+bool structureParser<streamT>::nextChar() {
+  // If there is another char in buf to read, advance to it
+  if(bufIdx<dataInBuf-1) {
+    bufIdx++;
+    return true;
+  // Otherwise, read more of the file into buf
+  } else {
+    // Read the next bufSize chunk of chars from the file. The return value is stored
+    // in bufSize to make sure that the caller is informed if readData() hits an error 
+    // or file end and doesn't fill buf up. When we finally consume all the bytes
+    // just read we'll reach the above feof() and ferror() tests and exit. Note that
+    // before this happens we may reach the terminating chars and exit/enter 
+    // readUntil() multiple times.
+    dataInBuf = readData();
+
+    // Reset bufIdx to refer to the start of buf
+    bufIdx=0;
+
+    if(dataInBuf>0) return true;
+  }
+
+  return false;
+}
+
+// Returns true if character c is in array termChars of size numTermChars and 
+// false otherwise.
+template<typename streamT>
+bool structureParser<streamT>::isMember(char c, const char* termChars, int numTermChars) {
+  for(int i=0; i<numTermChars; i++) {
+    // If c is found in termChars
+    if(c==termChars[i]) return true;
+  }
+  return false;
+}
+
+
+/*******************************
+ ***** FILEStructureParser *****
+ *******************************/
+
+FILEStructureParser::FILEStructureParser(string fName, int bufSize) : 
+  structureParser<FILE>(bufSize)
+{ 
+  FILE* f = fopen(fName.c_str(), "r");
+  if(f==NULL) { cerr << "ERROR opening file \""<<fName<<"\" for reading! "<<strerror(errno)<<endl; exit(-1); }
+  openedFile=true;
+  init(f);
+}
+
+FILEStructureParser::FILEStructureParser(FILE* f, int bufSize) : structureParser<FILE>(f, bufSize) {
+  openedFile=false;
+}
+
+FILEStructureParser::~FILEStructureParser() {
+  // If we opened the file, we must close it
+  if(openedFile)
+    fclose(stream);
+}
+
+// Functions implemented by children of this class that specialize it to take input from various sources.
+
+// readData() reads as much data as is available from the data source into buf[], upto bufSize bytes 
+// and returns the amount of data actually read.
+size_t FILEStructureParser::readData() {
+  return fread(buf, 1, bufSize, stream);
+}
+
+// Returns true if we've reached the end of the input stream
+bool FILEStructureParser::streamEnd() {
+  return feof(stream);
+}
+
+// Returns true if we've encountered an error in input stream
+bool FILEStructureParser::streamError() {
+  return ferror(stream);
+}
+
+} // namespace sight
---
+++ process.h
@@ -0,0 +1,140 @@
+#pragma once 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <map>
+#include <list>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <errno.h>
+#include "sight_common_internal.h"
+//#include "sight_layout.h"
+
+namespace sight {
+
+template<typename streamT>
+class structureParser {
+  protected:
+  // Holds the data just read from the data source
+  char* buf;
+  
+  // Allocated size of buf
+  size_t bufSize;
+  
+  // Number of bytes currently stored in buf
+  size_t dataInBuf;
+  
+  // The current index of the read pointer within buf[]
+  int bufIdx;
+  
+  // Reference to the data source
+  streamT* stream;
+  
+  // Functions implemented by children of this class that specialize it to take input from various sources.
+  
+  // readData() reads as much data as is available from the data source into buf[], upto bufSize bytes 
+  // and returns the amount of data actually read.
+  virtual size_t readData()=0;
+  
+  // Returns true if we've reached the end of the input strea
+  virtual bool streamEnd()=0;
+  
+  // Returns true if we've encountered an error in input stream
+  virtual bool streamError()=0;
+
+  public:
+  
+  // Differentiates between the entry tag of an object and its exit tag
+  typedef enum {enterTag, exitTag} tagType;
+  
+  structureParser(int bufSize=10000);
+  structureParser(streamT* stream, int bufSize=10000);
+  void init(streamT* stream);
+  
+  protected:
+  // The location within nextLoc() at which the last call to newLoc() stopped and the
+  // next newLoc() call will resume.
+  typedef enum {start,
+                textRead,
+                enterTagRead,
+                exitTagRead,
+                done} codeLoc;
+  codeLoc loc;
+  
+  // The properties of the object tag that is being currently read
+  properties tagProperties;
+  
+  
+  public:
+  // Reads more data from the data source, returning the type of the next tag read and the properties of 
+  // the object it denotes.
+  std::pair<tagType, const properties&> next();
+  
+  protected:
+  // Read a property name/value pair from the given file, setting name and val to them.
+  // Reading starts at buf[bufIdx] and continues as far as needed, reading more file 
+  // contents into buf if the end of buf is reached. bufSize is the number of bytes in 
+  // the buffer. When the function returns bufIdx is updated to hold the current 
+  // position inside buf, immediately after the terminator char was encountered and 
+  // bufSize is updated to hold the total number of characters currently in buf. 
+  // termChar is set to be the character that immediately follows the property name/value.
+  // pair. This information enables the caller to determine whether the tag is finished.
+  // or whether more properties exist in the tag. If readProperty() reads the full 
+  // property info before it reaches the end of the file, it returns true and false otherwise.
+  bool readProperty(std::string& name, std::string& val, char& termChar);
+  
+  // Reads all the characters from FILE* f until the next terminating character.
+  // If inTerm is true, a terminating char is anything in termChars. If inTerm is 
+  // false, a terminating char is anything not in termChars. readUntil() then sets 
+  // result to the read text and sets termHit to the terminator char that was hit. 
+  // Reading starts at buf[bufIdx] and continues as far as needed, reading more file 
+  // contents into buf if the end of buf is reached. bufSize is the number of bytes in 
+  // the buffer. When the function returns bufIdx is updated to hold the current 
+  // position inside buf, immediately after the terminator char was encountered and 
+  // bufSize is updated to hold the total number of characters currently in buf. If 
+  // readUntil() finds a terminator char before it reaches the end of the file, it 
+  // returns true and false otherwise.
+  bool readUntil(bool inTerm, const char* termChars, int numTermChars, 
+                 char& termHit, std::string& result);
+  
+  // Advances the current reading position by 1 char, reading file contents into buf 
+  // if the end of buf is reached. bufSize is the number of bytes in 
+  // the buffer. When the function returns bufIdx is updated to hold the current 
+  // position inside buf, immediately after the terminator char was encountered and 
+  // bufSize is updated to hold the total number of characters currently in buf. 
+  // Returns the read char. If there is a next character in the file, returns true.
+  // Otherwise, returns false.
+  bool nextChar();
+  
+  // Returns true if character c is in array termChars of size numTermChars and 
+  // false otherwise.
+  static bool isMember(char c, const char* termChars, int numTermChars);
+};
+
+
+class FILEStructureParser : public structureParser<FILE> {
+  // Records whether this object opened the file on its own (in which case it needs to close it)
+  // or was given a ready FILE* stream
+  bool openedFile;
+  
+  public:
+  FILEStructureParser(std::string fName, int bufSize=10000);
+  FILEStructureParser(FILE* f, int bufSize=10000);
+  ~FILEStructureParser();
+  
+  protected:
+  // Functions implemented by children of this class that specialize it to take input from various sources.
+  
+  // readData() reads as much data as is available from the data source into buf[], upto bufSize bytes 
+  // and returns the amount of data actually read.
+  size_t readData();
+  
+  // Returns true if we've reached the end of the input stream
+  bool streamEnd();
+  
+  // Returns true if we've encountered an error in input stream
+  bool streamError();
+};
+
+} // namespace sight
