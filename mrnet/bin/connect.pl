96b468c6f1ea8e1855b581f6c44b7f02cbdf450b
usw <usw@usw-VirtualBox.(none)>
2014-09-17 04:56:19 -0400

    fixing local connect.pl

--- mrnet/bin/connect.pl
+++ mrnet/bin/connect.pl
@@ -2,30 +2,33 @@
 
 sub connect_be {
         my $conn_str = shift;
-	my $sight_app ;
-	my $be_rank ;
+	my $be_host_name = shift;
+	my $sight_app = shift;
+	my $app_params = shift ;
+        
+  	#my $params_w_n = $app_params ;
 	my $sight_mrnet_env ;
-	my $be_host_name ;
 	my $cmd ;
         if($be_host_name == "localhost" || $be_host_name == "127.0.0.1" ){
 		#local BE
-		$sight_mrnet_env="smrnet_be $conn_str";
+		$sight_mrnet_env="$conn_str";
 		delete $ENV{'SIGHT_FILE_OUT'};
 		$ENV{'MRNET_MERGE_EXEC'} = $sight_mrnet_env ;
-		$cmd = $sight_app;		
+		$cmd = "$sight_app $app_params";		
+                print "$cmd -- $sight_mrnet_env -- $be_host_name  \n" ;  
+  		system("$cmd");
 	}else {
    		#remote BE - connect via ssh
 		#unset SIGHT_FILE_OUT;
 		#export MRNET_MERGE_EXEC="./smrnet_be 127.0.0.1 $PARENT_PORT $PARENT_RANK 127.0.0.1 $BE_RANK";
 		
 	}
-  	system($cmd);
 }
 
 my $TOP_FNAME = $ARGV[0] || 'connection.params' ;
-my $PARAM_FNAME = $ARGV[1] || 'file1.txt' ;
+my $PARAM_FNAME = $ARGV[1] || 'app.params' ;
 
-print "$TOP_FNAME $PARAM_FNAME \n";
+print "taking parameters from $TOP_FNAME : $PARAM_FNAME \n";
 
 open FILE, $TOP_FNAME or die $!;
 open P_FILE, $PARAM_FNAME or die $!;
@@ -35,12 +38,19 @@ my @params = <P_FILE> ;
 
 my $i = 0;
 my @children;
+my $rank = 10200;
 
 foreach my $line(@lines){
   @words = split(/ /, $line);
   @line_w_n = split(/\n/, $line);
 
-  $cmd = "./my_program.sh $line_w_n[0] $params[$i]";
+  @app_params = split(/ /, $params[$i]);
+
+  #parse application specific params
+  $bin_name = shift @app_params;
+  $be_hname = shift @app_params;
+
+  $conn_str = "smrnet_be $line_w_n[0] $be_hname $rank";
 #  system($cmd);
   my $pid = fork();
   if ($pid) {
@@ -48,13 +58,15 @@ foreach my $line(@lines){
 	push(@children, $pid); 
   } elsif($pid == 0){
  	#child
-        connect_be($cmd);
+ 	@tmp = split(/\n/,"@app_params") ;
+	#print "$tmp[0]\n";
+        connect_be($conn_str, $be_hname, $bin_name, $tmp[0]);
         exit 0 ;   
   } else{
         die "couldnt fork: $!\n";
   } 
-
   $i = $i + 1;
+  $rank = $rank + 1 ;
 }
 
 foreach (@children) {
