5894c9b5a4221378c3f9002881e861546dedd3f3
Greg Bronevetsky <bronevetsky@llnl.gov>
2014-10-18 21:32:32 -0700

    Updated the files includes in sight_hs.h and added the #defines that make it easy to turn SIGHT on and off

--- sight_ns.h
+++ sight_ns.h
@@ -7,5 +7,24 @@
 #include "widgets/module/module_structure.h"
 #include "widgets/source/source_structure.h"
 #include "widgets/clock/clock_structure.h"
+#include "widgets/parallel/parallel_structure.h"
+#include "widgets/box/box_api_cpp.h"
 
 namespace sightN = sight::structure;
+
+
+// Macro to make it easier to choose whether to enable or disable all instances of Sight objects and stream output in an application.
+// Objects are declared as SIGHT(scope, s, ("scope label", scope::high"))
+// Text output is emitted to stream sight: sight << "text"<<endl;
+// All of these can be eliminated from the application to minimize the runtime cost of Sight via
+// #define DISABLE_SIGHT
+//
+#ifdef DISABLE_SIGHT
+#define SIGHT(varType, varName, varParams)
+#define sght common::nullS
+#define SightInit NullSightInit
+#else
+#define SIGHT(varType, varName, varParams) varType varName varParams
+#define sght dbg
+#endif
+
